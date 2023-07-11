a1299aba18d8b7544e4c641f3bdafead0f76f081
bilmoleque2 <gabriel.bil8@gmail.com>
2023-07-11 12:20:08 -0300

    Rebase e Update do Vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
-export default function App() {
+import { useState } from 'react';
+import './App.css';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+
+function App() {
+	const [count, setCount] = useState(0);
+
 	return (
-		<section>
-			<h1>Em Desenvolvimento...</h1>
-		</section>
+		<div className='App'>
+			<div>
+				<a href='https://vitejs.dev' target='_blank'>
+					<img src={viteLogo} className='logo' alt='Vite logo' />
+				</a>
+				<a href='https://reactjs.org' target='_blank'>
+					<img src={reactLogo} className='logo react' alt='React logo' />
+				</a>
+			</div>
+			<h1>BM2_DEV</h1>
+			<div className='card'>
+				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+				<p>
+					Edit <code>src/App.tsx</code> and save to test HMR
+				</p>
+			</div>
+			<p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+		</div>
 	);
 }
+
+export default App;
